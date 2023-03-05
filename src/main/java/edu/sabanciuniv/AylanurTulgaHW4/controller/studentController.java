f623a7297c5ef799b5fe43dba3f061237ebb7624
Ayla Tulga <aylanur.tulga@sabanciuniv.edu>
2023-03-06 01:10:31 +0300

    updated.

--- src/main/java/edu/sabanciuniv/AylanurTulgaHW4/controller/studentController.java
+++ src/main/java/edu/sabanciuniv/AylanurTulgaHW4/controller/studentController.java
@@ -5,9 +5,7 @@ import edu.sabanciuniv.AylanurTulgaHW4.service.studentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class studentController {
